ab928d17ce59ecea02582533cecb8cefde3150f7
KHANDOLKAR-SOMESH <141264285+KHANDOLKAR-SOMESH@users.noreply.github.com>
2025-02-22 18:15:56 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -7,7 +7,11 @@ npm install
 
 # Ensure Puppeteer's cache directory exists
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+PROJECT_CACHE_DIR=/opt/render/project/src/.cache/puppeteer
+
+# Create directories if they don't exist
 mkdir -p $PUPPETEER_CACHE_DIR
+mkdir -p $PROJECT_CACHE_DIR
 
 # Install Puppeteer and download Chromium
 npx puppeteer install
@@ -15,8 +19,8 @@ npx puppeteer install
 # Store/pull Puppeteer cache with build cache
 if [[ -d $PUPPETEER_CACHE_DIR ]]; then
   echo "Storing Puppeteer Cache in Build Cache"
-  cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/
+  cp -R $PUPPETEER_CACHE_DIR/* $PROJECT_CACHE_DIR/
 else
   echo "Copying Puppeteer Cache from Build Cache"
-  cp -R /opt/render/project/src/.cache/puppeteer/ $PUPPETEER_CACHE_DIR
+  cp -R $PROJECT_CACHE_DIR/* $PUPPETEER_CACHE_DIR/
 fi
